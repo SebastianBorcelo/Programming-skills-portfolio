9493d050c36f627ba2c71c36d3be3158189b313e
SebastianBorcelo <sebastian.borcelo@gmail.com>
2024-11-24 22:02:30 +0300

    Excercise 07.py

---
+++ Excercise 07 - Simple counting/Excercise 07.py
@@ -0,0 +1,22 @@
+#Seventh excercise
+
+print("0 to 50")
+for i in range(0,51,1):
+    print(i)
+
+print("50 to 0")
+for i in range(50,-1,-1):
+    print(i)
+
+print("30 to 50")
+for i in range(30,51,1):
+    print(i)
+
+
+print("50 to 10")
+for i in range(50,8,-2):
+    print(i)
+
+print("100 to 200")
+for i in range(100,201,5):
+  print(i)
